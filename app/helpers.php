8e5dd92f31a6f9f6a3fe70ab81eb359e29bf1dd3
Yassine Ben Slimane <yuginim@gmail.com>
2018-02-09 17:19:02 +0100

    Added method to generate a random hex color.

--- app/helpers.php
+++ app/helpers.php
@@ -1,13 +1,25 @@
 <?php
 
 if (! function_exists('ddd')) {
-    
+
     /**
      * workround for the laravel helper function dd
+     * @param array $args
      */
     function ddd(...$args)
     {
         http_response_code(500);
         call_user_func_array('dd', $args);
+    }
+}
+
+if (! function_exists('random_color')) {
+
+    /**
+     * workround for the laravel helper function dd
+     */
+    function random_color()
+    {
+        return '#' . dechex(rand(0x000000, 0xFFFFFF));
     }
 }
