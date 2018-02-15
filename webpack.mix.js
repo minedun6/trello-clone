b54b9e89be7d9721ea41a1247f8ae7072c131721
Yassine Ben Slimane <yuginim@gmail.com>
2018-02-15 09:51:34 +0100

    Added laravel-mix-purgecss to remove unused css rules.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,11 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .disableNotifications()
+    .purgeCss()
     .options({
         processCssUrls: false,
         postCss: [ tailwindcss('./tailwind.js') ],
